ae6df7e1742443a3679adac36afb69cfc42205a5
Hitfot <hitfot@mail.ru>
2022-04-11 11:57:45 +0300

    ы

--- lib/presentaion/pages/profile/profile_page.dart
+++ lib/presentaion/pages/profile/profile_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:vdole_mobile/presentaion/colors.dart';
 
-class ProfilePage extends StatelessWidget{
-  const ProfilePage({Key? key}) : super(key: key);
+class ProfilePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State{
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context){
@@ -14,11 +21,34 @@ class ProfilePage extends StatelessWidget{
           children: <Widget>[
             Container(
               margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-              child: const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Введите Email',
+              child: TextFormField(
+                key: _formKey,
+                style: const TextStyle(color: DarkThemeColors.deactive),
+                decoration: const InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: DarkThemeColors.deactive,
+                      )
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: DarkThemeColors.deactive,
+                      )
+                  ),
+                  hintText: "Введите Email",
+                  hintStyle: TextStyle(color: DarkThemeColors.deactive),
+                  //fillColor: DarkThemeColors.tinkbg01,
                 ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+              color: Colors.green,
+              child: TextButton(
+                onPressed: (){
+                  if(_formKey.currentState.)
+                },
+                child: Text("Отправить", style: TextStyle(color: DarkThemeColors.tinkbg00),),
               ),
             )
           ]
